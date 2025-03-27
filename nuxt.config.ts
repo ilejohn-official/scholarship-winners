5448d38d4fe0a5ac348504e14ade9bdacb21034a
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-03-27 15:55:34 +0100

    set up ssr and base api path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: true, // Enables Server-Side Rendering
+  typescript: {
+    strict: true,
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: "https://scholarshipowl.com/jsonapi",
+    },
+  },
 })
