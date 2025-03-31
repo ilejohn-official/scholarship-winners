58bba30d62537daf4b835bfe2f1ceacc3b6c83f3
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-03-31 22:31:01 +0100

    rename types and api to use standard naming

--- server/api/winners.get.ts
+++ server/api/winners.get.ts
@@ -1,6 +1,6 @@
-import { WinnersResponse } from '~/types/winners';
+import type { WinnersResponse } from '~/shared/types/winners';
 
-// Updated handler with type safety and base URL from runtime config
+
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
   const baseUrl = config.public.apiBaseUrl;
@@ -8,9 +8,8 @@ export default defineEventHandler(async (event) => {
   try {
     const query = getQuery(event); // Extract query parameters
     const limit = query.limit ? Number(query.limit) : 10; // Default to 10 records
-    const response: WinnersResponse = await $fetch(`${baseUrl}/winner?page[limit]=${limit}`);
 
-    return response;
+    return await $fetch<WinnersResponse>(`${baseUrl}/winner?page[limit]=${limit}`);
   } catch (error) {
     throw createError({ statusCode: 500, statusMessage: "Failed to fetch winners" });
   }
