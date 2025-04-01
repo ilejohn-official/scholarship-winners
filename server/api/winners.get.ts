6c196b764ad6f868c697905c1863bbfb9fc9a865
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-04-01 19:14:15 +0100

    parse url query

--- server/api/winners.get.ts
+++ server/api/winners.get.ts
@@ -7,12 +7,17 @@ export default defineEventHandler(async (event) => {
 
   try {
     const query = getQuery(event);
-    const limit = Number(query.limit) || 10;
-    const page = Number(query.page) || 1;
+    const limit: number = Number(query.limit) || 10;
+    const page: number = Number(query.page) || 1;
+    const winnerFields: string = query.fields ? String(query?.fields) : 'winnerPhoto,winnerName,scholarshipTitle,amountWon,wonAt';
 
-    return await $fetch<WinnersResponse>(`${baseUrl}/winner`, {
-      query: { "page[number]": page, "page[limit]": limit }
+    const queryParams = new URLSearchParams({
+      "page[number]": String(page),
+      "page[limit]": String(limit),
+      "fields[winner]": winnerFields
     });
+
+    return await $fetch<WinnersResponse>(`${baseUrl}/winner?${queryParams.toString()}`);
   } catch (error) {
     throw createError({ statusCode: 500, statusMessage: "Failed to fetch winners" });
   }
