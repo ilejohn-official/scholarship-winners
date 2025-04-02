78480aac152b392893308978ecc6d5aa51831d4e
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-04-02 18:04:10 +0100

    use template destructuring for better readability

--- components/WinnerTable.vue
+++ components/WinnerTable.vue
@@ -2,42 +2,42 @@
   <DataTable :value="winners" class="p-datatable-striped">
     <!-- Winner Photo -->
     <Column header="Photo">
-      <template #body="{ data }">
+      <template #body="{ data: { attributes } }">
         <Skeleton v-if="tableLoading" width="50px" height="50px" borderRadius="50%" />
-        <Image v-else :src="data.attributes.winnerPhoto" alt="Winner Photo" width="50" height="50" borderRadius="50%"
+        <Image v-else :src="attributes.winnerPhoto" alt="Winner Photo" width="50" height="50" borderRadius="50%"
           preview />
       </template>
     </Column>
 
     <!-- Winner Name -->
     <Column field="attributes.winnerName" header="Winner Name">
-      <template #body="{ data }">
+      <template #body="{ data: { attributes } }">
         <Skeleton v-if="tableLoading" width="100px" />
-        <span v-else>{{ data.attributes.winnerName }}</span>
+        <span v-else>{{ attributes.winnerName }}</span>
       </template>
     </Column>
 
     <!-- Scholarship Title -->
     <Column field="attributes.scholarshipTitle" header="Scholarship Title">
-      <template #body="{ data }">
+      <template #body="{ data: { attributes } }">
         <Skeleton v-if="tableLoading" width="150px" />
-        <span v-else>{{ data.attributes.scholarshipTitle }}</span>
+        <span v-else>{{ attributes.scholarshipTitle }}</span>
       </template>
     </Column>
 
     <!-- Amount Won -->
     <Column field="attributes.amountWon" header="Amount Won">
-      <template #body="{ data }">
+      <template #body="{ data: { attributes } }">
         <Skeleton v-if="tableLoading" width="80px" />
-        <span v-else>${{ data.attributes.amountWon.toLocaleString() }}</span>
+        <span v-else>${{ attributes.amountWon.toLocaleString() }}</span>
       </template>
     </Column>
 
     <!-- Date Won -->
     <Column field="attributes.wonAt" header="Date Won">
-      <template #body="{ data }">
+      <template #body="{ data: { attributes } }">
         <Skeleton v-if="tableLoading" width="100px" />
-        <span v-else>{{ formatDate(data.attributes.wonAt) }}</span>
+        <span v-else>{{ formatDate(attributes.wonAt) }}</span>
       </template>
     </Column>
   </DataTable>
